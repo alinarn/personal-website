65162d8c3d1a220a9283d59a91b45a9b61073b8d
Alina A <alinarnautova@gmail.com>
2024-07-17 18:58:26 -0400

    change text weight

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,13 +4,13 @@ import { ReactTyped } from 'react-typed';
 function Intro() {
     return (
     <div className="intro">
-        <h1> 
+      <h1 className="typed-text"> 
         hi, {" "}
         <ReactTyped
           strings={["I'm Alina"]}
           typeSpeed={150}/>
-     </h1>
-     <h4>currently building software in New York</h4>
+       </h1>
+     <h4 className="subheader">currently building software in New York</h4>
     </div>
     )
 }
