624ec11e9e3e002cbf6547d81a8d6a051f441f08
Alina A <alinarnautova@gmail.com>
2024-07-17 18:30:53 -0400

    add links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Tree from './components/Tree.js';
 import Intro from './components/Intro.js';
+import Links from './components/Links.js';
 
 function App() {
   return (
     <div className="App">
       <div><Tree /></div>
       <div><Intro /></div>
+      <div><Links /></div>
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Links = () => {
+    return (
+      <div className="icons-section">
+        <a href="https://github.com/alinarn" className="icon" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/alina-arnautova/" className="icon" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </div>
+    );
+  };
+
+  export default Links;
